e68dfce85bcfe4ca9dcdd42a3c66bf52a52f9a27
Homespeaker <142523511+Homespeaker@users.noreply.github.com>
2024-03-27 22:02:29 +0300

    Update commands.py

--- commands.py
+++ commands.py
@@ -6,7 +6,4 @@ def create_new_token():
     metadata_url = "http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token"
     headers = {"Metadata-Flavor": "Google"}
     response = requests.get(metadata_url, headers=headers)
-    print(response.json())
-    print(response.json()["access_token"])
-    new_iam_token(f"{response.json()['access_token']}")
-    timez_new((response.json())["expires_in"])
+    return response.json()
